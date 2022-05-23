e3ed962e29fd509d846b535fdb1006f0aa34fcb0
TimK76 <haadelphos2001@gmail.com>
2022-05-23 08:17:12 -0400

    added user controller and server.js and ready to start testing user routes in Insomnia

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,11 +1,77 @@
+const { User } = require('../models');
 
+const userController = {
+getAllUser(req, res) {
+    User.find({})
+    .populate({
+      path: 'thoughts',
+      select: '-__v'
+    })
+    .select('-__v')
+    .sort({ _id: -1 })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+},
+getUserById(req, res) {
+    User.findOne({ _id: params.id })
+    .populate({
+      path: 'thoughts',
+      select: '-__v'
+    })
+    .select('-__v')
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+},
+createUser(req, res) {
+    User.create(body)
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => res.json(err));
+},
+updateUser(req, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No User found with this id!' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => res.status(400).json(err));
+},
+deleteUser(req, res) {
+    User.findOneAndDelete({ _id: params.id })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => res.json(err));
+},
+addFriend({ params, body }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $push: {friends: body }},
+      {new: true, runValidators: true}
+    )
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id!"});
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => res.json(err));
+  },
 
-
-
-getAllUser,
-getUserById,
-createUser,
-updateUser,
-deleteUser,
-addFriend,
-deleteFriend
+deleteFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: {replies: {friendId: params.friendId}}},
+      { new: true},
+    )
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => res.json(err));
+  }
+};
